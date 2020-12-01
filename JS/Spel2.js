34c7217c555e5cca0f4f3faad29ec9d9b09b3f6c
JustYourAverageIdiot <151667@csgaugustinus.nl>
2020-12-01 09:06:41 +0000

    yeet

--- JS/Spel2.js
+++ JS/Spel2.js
@@ -1,7 +1,10 @@
 var xPlayer = 0 ;
 var yPlayer = 375 ;
 var Jump = 5 ; 
+var Run = 5 ; 
 var BGColor = 'silver';
+var PlayerColor = 'black';
+var RightArmColor = 'silver';
 
 function setup() {
   canvas = createCanvas(450,450);
@@ -14,14 +17,44 @@ function draw() {
     noStroke();
     fill ('black');
     rect(0,425,450,25);
+    fill (PlayerColor);
     rect(xPlayer,yPlayer,50,50);
+    fill(RightArmColor)
+    rect(xPlayer + 50,yPlayer + 15,30,20)
 
-    if (keyIsDown(LEFT_ARROW)) {
-        xPlayer -= 5;
+    if (keyIsDown(65)) {
+        xPlayer -= Run;
+    }
+
+    if (keyIsDown(68)) {
+        xPlayer += Run;
+    }
+
+    if (keyIsDown(87)) {
+        yPlayer -= 5;
+    }
+
+    if (keyIsDown(83)) {
+        yPlayer += 5;
+    }
+
+    if (keyIsDown(16)) {
+        Run = 10 ; 
+        PlayerColor = 'red';
+    }
+
+    else {
+        Run = 5 ; 
+        PlayerColor = 'black';
     }
 
     if (keyIsDown(RIGHT_ARROW)) {
-        xPlayer += 5;
+        RightArmColor = PlayerColor;
     }
+
+    else {
+        RightArmColor = 'silver';
+    }
+
 
 }
